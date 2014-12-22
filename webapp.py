39e3f81705236704c68743bb1d91f0729e9ab9c7
Eli Finer <eli.finer@gmail.com>
2014-12-22 15:31:21 +0200

    improved api

--- webapp.py
+++ webapp.py
@@ -1,33 +1,32 @@
-from flask import Flask
+from flask import Flask, request, abort
 from jinja2 import Template
 app = Flask(__name__)
 
 from leumi import BankLeumiAPI
-import config
 
 TEMPLATE = '''
 <style>
-body { direction: rtl; }
-table { border-collapse: collapse; }
-td { padding: 5px; border: 1px solid #ccc; }
+    body { direction: rtl; }
+    table { border-collapse: collapse; }
+    td { padding: 5px; border: 1px solid #ccc; }
 </style>
 <body>
 <table>
-    {% for row in rows %}
-    <tr>
-        {% for cell in row[:6] %}
-        <td>{{cell}}</td>
-        {% endfor %}
-    </tr>
-    {% endfor %}
+    {%- for row in rows %}
+    <tr>{% for cell in row[:6] %}<td>{{cell}}</td>{% endfor %}</tr>
+    {%- endfor %}
 </table>
 </body>
 '''
 
-@app.route('/api/bank_leumi/<account>')
+@app.route('/api/bank_leumi/<account>', methods=['GET', 'POST'])
 def bank_leumi(account):
+    username = request.values.get('username')
+    password = request.values.get('password')
+    if not username or not password:
+        abort(400)
     api = BankLeumiAPI()
-    api.login(config.LEUMI_USERNAME, config.LEUMI_PASSWORD)
+    api.login(username, password)
     template = Template(TEMPLATE)
     rows = api.get_statement(None,None,None)
     return template.render(rows=rows)
